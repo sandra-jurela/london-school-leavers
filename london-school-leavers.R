0b8dea8e690d76902d1623498bfeac7999b49b1e
Sandra Jurela <93125146+sandra-jurela@users.noreply.github.com>
2022-12-05 16:10:06 +0100

    Add files via upload

---
+++ london-school-leavers.R
@@ -0,0 +1,32 @@
+# loading packages
+library(tidyverse)
+library(sf)
+
+# reading data
+london_sf <- read_sf("data/london_boroughs")
+
+education_data <- read_csv("data/age-when-completed-education.csv")
+
+
+# data wrangling
+london_school_leavers_sf <- london_sf %>% 
+  left_join(education_data,
+            by = c("lad11nm" = "area")) %>% 
+  filter(age_group == "16 or under")
+
+
+# data visualization
+ggplot() +
+  geom_sf(data = london_school_leavers_sf,
+          aes(fill = value,
+              shape = "City of London"),
+          color = "white",
+          size = 0.2) +
+  scale_fill_viridis_c(labels = scales::number_format(big.mark = ","),
+                       name = "School leavers\n16 or under",
+                       na.value = "pink") +
+  guides(shape = guide_legend(override.aes = list(fill = "pink"),
+                              title = NULL,
+                              order = 2),
+         fill = guide_colorbar(order = 1)) +
+  theme_void()
